1a30d6d626a3a895a1040f22ee2d3f50903ea8ce
thomas huynh <thomas.huynh@ruggable.com>
2023-04-13 19:10:31 -0700

    WIP angle of clicking circle

---
+++ dist/src.a2b27638.js
@@ -0,0 +1,398 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"src/index.js":[function(require,module,exports) {
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath(); // start a new path
+var ballRadius = 12;
+var clickBubbleRadius = 6;
+// set ball in middle;
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+var clickX = -50;
+var clickY = -50;
+var clickAlpha = 1;
+var fadeOutRate = .03;
+
+// initial velocity
+var dx = 2;
+var dy = -2;
+var energy = 1;
+function drawClickBubble(e) {
+  if (e) {
+    clickX = e.offsetX;
+    clickY = e.offsetY;
+    clickAlpha = 1;
+  }
+  ctx.beginPath();
+  ctx.arc(clickX, clickY, clickBubbleRadius, 0, 2 * Math.PI);
+  clickAlpha -= fadeOutRate;
+  ctx.globalAlpha = clickAlpha >= 0 ? clickAlpha : 0;
+  ctx.fillStyle = "pink";
+  ctx.fill(); // fill the circle with the current fill color
+  ctx.closePath();
+}
+canvas.addEventListener("click", function (e) {
+  drawClickBubble(e);
+  clickBall();
+});
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, 2 * Math.PI); // draw a circle with center at (25, 25) and radius of 25 pixels
+  ctx.globalAlpha = 1;
+  ctx.fillStyle = "red";
+  ctx.fill(); // fill the circle with the current fill color
+  ctx.closePath();
+}
+function calculateMovement() {
+  if (energy > 0) {
+    energy -= 0.01;
+  }
+  x += dx * energy;
+  y += dy * energy;
+
+  // flip direction if ball reaches edge of canvas
+  if (x < 0 + ballRadius || x > canvas.width - ballRadius) {
+    dx = -dx;
+  }
+  if (y < 0 + ballRadius || y > canvas.height - ballRadius) {
+    dy = -dy;
+  }
+}
+function clickBall() {
+  var clickToCircleX = clickX - x;
+  var clickToCircleY = clickY - y;
+  var distance = Math.sqrt(clickToCircleX * clickToCircleX + clickToCircleY * clickToCircleY); // sqrt(x1 * x2 + y1 * y2) - distance formula
+  if (distance > ballRadius) {
+    return;
+  }
+  // calculate the angle between the click position and the circle center
+  var angle = Math.atan2(clickToCircleY, clickToCircleX);
+  console.log({
+    angle: angle,
+    pi: Math.PI
+  });
+
+  // move the circle depending on where the user clicks it
+  if (angle < -Math.PI / 4 && angle >= -3 * Math.PI / 4) {
+    // x += distance * Math.cos(Math.PI / 4 + angle);
+    // y -= distance * Math.sin(Math.PI / 4 + angle);
+    dx = Math.abs(dx);
+    dy = -Math.abs(dy);
+    console.log('bottom-left');
+  } else if (angle >= -Math.PI / 4 && angle < Math.PI / 4) {
+    // x += distance;
+    dx = -Math.abs(dx);
+    dy = -Math.abs(dx);
+    console.log('bottom-right');
+  } else if (angle >= Math.PI / 4 && angle < 3 * Math.PI / 4) {
+    // x += distance * Math.cos(Math.PI / 4 - angle);
+    // y += distance * Math.sin(Math.PI / 4 - angle);
+    dy = Math.abs(dy);
+    dx = Math.abs(dx);
+    console.log('bottom');
+  } else {
+    // x -= distance;
+    console.log('bottom-left');
+    dx = Math.abs(dx);
+    dy = -Math.abs(dx);
+  }
+  energy = 1;
+}
+function animate() {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawClickBubble();
+  calculateMovement();
+  requestAnimationFrame(animate);
+}
+animate();
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61420" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
+//# sourceMappingURL=/src.a2b27638.js.map
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const ctx = canvas.getContext("2d");
 
 ctx.beginPath(); // start a new path
 const ballRadius = 12;
-const clickBubbleRadius = 4;
+const clickBubbleRadius = 6;
 // set ball in middle;
 let x = canvas.width / 2;
 let y = canvas.height / 2;
 
-let clickX = 0;
-let clickY = 0;
+let clickX = -50;
+let clickY = -50;
+let clickAlpha = 1;
+const fadeOutRate = .03;
 
 // initial velocity
 let dx = 2;
@@ -20,37 +22,84 @@ function drawClickBubble(e) {
   if (e) {
     clickX = e.offsetX;
     clickY = e.offsetY;
+    clickAlpha = 1;
   }
   ctx.beginPath();
   ctx.arc(clickX, clickY, clickBubbleRadius, 0, 2 * Math.PI);
+  clickAlpha -= fadeOutRate;
+  ctx.globalAlpha = clickAlpha >= 0 ?  clickAlpha : 0;
   ctx.fillStyle = "pink";
   ctx.fill(); // fill the circle with the current fill color
   ctx.closePath();
 }
 
-canvas.addEventListener("click", drawClickBubble);
+canvas.addEventListener("click", (e) => {
+  drawClickBubble(e);
+  clickBall();
+});
 
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, 2 * Math.PI); // draw a circle with center at (25, 25) and radius of 25 pixels
+  ctx.globalAlpha = 1;
   ctx.fillStyle = "red";
   ctx.fill(); // fill the circle with the current fill color
   ctx.closePath();
 }
 
 function calculateMovement() {
-  // flip direction if ball reaches edge of canvas
   if (energy > 0) {
     energy -= 0.01;
   }
   x += dx * energy;
   y += dy * energy;
+
+  // flip direction if ball reaches edge of canvas
   if (x < 0 + ballRadius || x > canvas.width - ballRadius) {
     dx = -dx;
   }
   if (y < 0 + ballRadius || y > canvas.height - ballRadius) {
     dy = -dy;
   }
+}
+
+function clickBall() {
+  const clickToCircleX = clickX - x;
+  const clickToCircleY = clickY - y;
+  const distance = Math.sqrt(clickToCircleX * clickToCircleX + clickToCircleY * clickToCircleY); // sqrt(x1 * x2 + y1 * y2) - distance formula
+  if (distance > ballRadius) {
+    return;
+  }
+  // calculate the angle between the click position and the circle center
+  const angle = Math.atan2(clickToCircleY, clickToCircleX);
+
+  console.log({ angle, pi: Math.PI })
+
+  // move the circle depending on where the user clicks it
+  if (angle < -Math.PI / 4 && angle >= -3 * Math.PI / 4) {
+    // x += distance * Math.cos(Math.PI / 4 + angle);
+    // y -= distance * Math.sin(Math.PI / 4 + angle);
+    dx = Math.abs(dx)
+    dy = -Math.abs(dy)
+    console.log('bottom-left')
+  } else if (angle >= -Math.PI / 4 && angle < Math.PI / 4) {
+    // x += distance;
+    dx = -Math.abs(dx)
+    dy = -Math.abs(dx)
+    console.log('bottom-right')
+  } else if (angle >= Math.PI / 4 && angle < 3 * Math.PI / 4) {
+    // x += distance * Math.cos(Math.PI / 4 - angle);
+    // y += distance * Math.sin(Math.PI / 4 - angle);
+    dy = Math.abs(dy);
+    dx = Math.abs(dx)
+    console.log('bottom')
+  } else {
+    // x -= distance;
+    console.log('bottom-left')
+    dx = Math.abs(dx)
+    dy = -Math.abs(dx)
+  }
+  energy = 1;
 }
 
 function animate() {
